0b06decb4e977196f1c3d86e03857fdac46e292c
Han123su <popsmall212@gmail.com>
2024-07-16 13:59:46 +0800

    modify

--- backend/Yolov7_StrongSORT_OSNet/strong_sort/deep/reid/torchreid/utils/feature_extractor.py
+++ backend/Yolov7_StrongSORT_OSNet/strong_sort/deep/reid/torchreid/utils/feature_extractor.py
@@ -4,10 +4,15 @@ import torch
 import torchvision.transforms as T
 from PIL import Image
 
-from .torchreid.utils import (
-    check_isfile, load_pretrained_weights, compute_model_complexity
-)
-from .torchreid.models import build_model
+#from torchreid.utils import (
+#    check_isfile, load_pretrained_weights, compute_model_complexity
+#)
+
+from .torchtools import load_pretrained_weights
+from .tools import check_isfile
+import .model_complexity import compute_model_complexity
+
+from torchreid.models import build_model
 
 
 class FeatureExtractor(object):
